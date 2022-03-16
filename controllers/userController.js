0cbfcdce80729e399e380aa176a1fd76c555b304
Colin Mosi Cargile <cargile10@gmail.com>
2022-03-15 22:27:35 -0400

    finished the controller for the users get all, getSingle, create user and delete user routes, still need to figure out how to add a thought and delete a thought

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,12 @@
 const { User, Thought } = require('../models');
 
 module.exports = {
-    getUsers(req, res) {
+    getUsers(req, res) { //gets all users 
         User.find()
             .then((users) => res.json(users))
             .catch((err) => res.status(500).json(err));
     },
-    getSingleUser(req, res) {
+    getSingleUser(req, res) { //gets a single user with an id
         User.findOne({ _id: req.params.userId })
             .select('-_v')
             .then((user) => 
@@ -16,9 +16,23 @@ module.exports = {
             )
             .catch((err) => res.status(500).json(err));
     },
-    createUser(req, res) {
+    createUser(req, res) { //creates a user 
         User.create(req.body)
             .then((user) => res.json(user))
             .catch((err) => (500).json(err));
     },
-}
+    deleteUser(req, res) { //deletes a user and their thoughts associated with the user
+        User.findOneAndDelete({ _id: req.params.userId })
+            .then((user) => 
+                !user 
+                    ? res.status(404).json({ message: 'No user with that ID exists'})
+                    : Thought.deleteMany({ _id: {$in: user.thoughts } })
+            )
+            .then(() => res.json({ message: 'User and associated thoughts deleted!'}))
+            .catch((err) => res.status(500).json(err));
+    },
+};
+
+//add a thought ??
+//remove a thought ??
+
