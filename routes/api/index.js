e79f09817048e6fd506e9380e73d05400e52996c
Colin Mosi Cargile <cargile10@gmail.com>
2022-03-17 13:19:13 -0400

    updated the user routes js file in routes and the index js file

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const userRoutes = require('./userRoutes');
+const userRoutes = require('./userRoutes'); //what is wrong with this line ?
 const thoughtRoutes = require('./thoughtRoutes');
 
 router.use('/users', userRoutes);
